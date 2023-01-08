905aa3ef8bbc7acdcce7717c02db6248064f81b4
aladzeika <aladzeika@spribe.co>
2023-01-08 22:20:05 +0300

    initial commit

---
+++ src/main/java/com/masterscam/tape/TapeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.masterscam.tape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapeCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masterscam/tape/TapeCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterscam.tape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapeCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
